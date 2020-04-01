ff447cb9fbfca36e6520ba18968e3013fe210799
João Dal Lago <50213926+jgdlago@users.noreply.github.com>
2020-04-01 07:23:16 -0300

    Add files via upload
    Estrutura de dados -> conversão de números binários para decimal stack

---
+++ convertDecToBin.py
@@ -0,0 +1,19 @@
+print('Número: ')
+numDec = input()
+
+def dec2bin(numDec):
+    b = ''
+    while numDec != 0:
+        if n == 0:
+            return ''
+        else:
+            d2b(n//2) + str(n%2)
+            divmod(numDec)
+    return print(b[::-1])
+
+    def push(self, numDec): # metodo de inserir item
+        if not self.is_empty():
+            self._vet.append(numDec)
+
+dec2bin(numDec)
+push()
